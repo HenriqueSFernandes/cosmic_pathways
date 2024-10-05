6ad46339a025561e68b266ee6cc011e23a84d470
avareno <miguelcabral798@gmail.com>
2024-10-05 23:48:31 +0100

    sound entrance

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 import * as THREE from "three";
+import AudioHandler from "./controllers/sound_class.js"; // Import your AudioHandler
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
-	75,
-	window.innerWidth / window.innerHeight,
-	0.1,
-	1000,
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+// Instantiate the AudioHandler
+const audioHandler = new AudioHandler(camera, "../sound/galaxy_2.mp3");
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
@@ -19,9 +23,14 @@ scene.add(cube);
 camera.position.z = 5;
 
 function animate() {
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-	renderer.render(scene, camera);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
 }
 
 renderer.setAnimationLoop(animate);
+
+// // Example of using setTimeout to change window location
+// setTimeout(() => {
+//     window.location.href = "https://example.com"; // Ensure correct property name
+// }, 5000); // Change to your desired URL and time
